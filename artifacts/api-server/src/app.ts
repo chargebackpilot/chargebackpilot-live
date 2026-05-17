830a2e8e9db6be72483ddd99cb862f2e5e708330
chargebackpilot <lote.tagsueber.0j@icloud.com>
2026-05-17 16:54:13 +0000

    Fix Express wildcard route for Render

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -38,7 +38,7 @@ const __dirname = path.dirname(__filename);
 const staticDir = path.resolve(__dirname, "../..", "chargeback-pilot", "dist", "public");
 
 app.use(express.static(staticDir));
-app.get("/*", (_req, res) => {
+app.get(/(.*)/, (_req, res) => {
   res.sendFile(path.join(staticDir, "index.html"));
 });
 
